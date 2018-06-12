1c3fde6016a9344d07cc88d238508ec595092bb8
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2018-06-12 18:11:22 +0100

    finished the object literal pattern

--- DesignPatterns/ObjectLiteralPattern/modularJS.js
+++ DesignPatterns/ObjectLiteralPattern/modularJS.js
@@ -1,39 +1,67 @@
-// var people = [];
-// var template = $('#people-template').html();
-
-
-// $('#peopleModule').find('button').on('click',function() {
-//     people.push($('#peopleModule').find('input').val());
-
-//     $('#peopleModule').find('input').val('');
-//     //data for mustach template
-//     var data = {
-//         people : people
-//     };
-//     $('#peopleModule').find('ul').html(Mustache.render(template,data));
-// });
-
-// $('#peopleModule').find('ul').delegate('i.del', 'click', function(e){
-//     var $remove = $(e.target).closest('li');
-//     var i =  $('#peoleModule').find('ul').find('li').index($remove);
-
-//     $remove.remove();
-
-//     people.splice(i,1);
-// });
-
-//OBJECT LITERAL PATTERN
-var myModule = {
-    name: "Will",
-    age: 34,
-    sayName: function(){
-       alert(this.name);
+// OBJECT LITERAL PATTERN
+    var people = {
+    people: ["Will", "Laura"],
+  
+    init: function() {
+        this.cacheDom(); 
+        this.bindEvents(); 
+        this.render();    
     },
-    setName: function(newName) {
-        this.name = newName;
+    cacheDom: function(){
+        this.$el = $('#peopleModule');
+        this.$button = this.$el.find('button');
+        this.$input = this.$el.find('input');
+        this.$ul = this.$el.find('ul');
+        this.$icon = this.$el.find('i');
+        this.template = this.$el.find('#people-template').html();
+    },
+    bindEvents: function() {
+        this.$button.on('click', this.addPerson.bind(this));
+        this.$ul.delegate('i.del','click', this.deletePerson.bind(this));
+    },
+    render: function() {
+        var data=  {
+            people: this.people
+        };
+        this.$ul.html(Mustache.render(this.template, data));
+    },
+    addPerson: function(value) {
+         this.people.push( value || this.$input.val());
+         this.render();
+         this.$input.val('');
+    },
+    deletePerson: function(event) {
+         var $remove=  $(event.target).closest('li');
+         var i = this.$ul.find('li').index($remove);
+         this.people.splice(i,1);
+         this.render();
     }
-}
+};
 
-myModule.sayName();
-myModule.setName("Dragomir");
-myModule.sayName();
+people.init();
+
+// (function(){
+//     $('#peopleModule').find('button').on('click',function() {
+//         people.push($('#peopleModule').find('input').val());
+    
+//         $('#peopleModule').find('input').val('');
+//         //data for mustach template
+//         var data = {
+//             people : people
+//         };
+//         $('#peopleModule').find('ul').html(Mustache.render(template,data));
+//     });
+    
+//     $('#peopleModule').find('ul').delegate('i.del', 'click', function(e){
+//         var $remove = $(e.target).closest('li');
+//         var i =  $('#peoleModule').find('ul').find('li').index($remove);
+    
+//         $remove.remove();
+    
+//         people.splice(i,1);
+//     });
+// })()
+
+
+
+
