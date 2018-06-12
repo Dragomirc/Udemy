8cc5ff487c02a2800f3f0828331eb2876552d4cd
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2018-06-12 17:12:16 +0100

    Added the object literal pattern

---
+++ DesignPatterns/ObjectLiteralPattern/modularJS.js
@@ -0,0 +1,39 @@
+// var people = [];
+// var template = $('#people-template').html();
+
+
+// $('#peopleModule').find('button').on('click',function() {
+//     people.push($('#peopleModule').find('input').val());
+
+//     $('#peopleModule').find('input').val('');
+//     //data for mustach template
+//     var data = {
+//         people : people
+//     };
+//     $('#peopleModule').find('ul').html(Mustache.render(template,data));
+// });
+
+// $('#peopleModule').find('ul').delegate('i.del', 'click', function(e){
+//     var $remove = $(e.target).closest('li');
+//     var i =  $('#peoleModule').find('ul').find('li').index($remove);
+
+//     $remove.remove();
+
+//     people.splice(i,1);
+// });
+
+//OBJECT LITERAL PATTERN
+var myModule = {
+    name: "Will",
+    age: 34,
+    sayName: function(){
+       alert(this.name);
+    },
+    setName: function(newName) {
+        this.name = newName;
+    }
+}
+
+myModule.sayName();
+myModule.setName("Dragomir");
+myModule.sayName();
