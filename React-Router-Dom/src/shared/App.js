2c24e17dbc32b35bd630818954461cffe6599935
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2018-06-20 18:23:56 +0100

    updated the React-Router folder name

---
+++ React-Router-Dom/src/shared/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import Grid from './Grid'
+class App extends Component {
+  render() {
+    return (<div><Grid data={this.props.data} /></div>)
+  }
+}
+
+export default App;
