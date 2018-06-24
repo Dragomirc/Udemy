eb9551a5b3c312442d5d5d3634ae9bb1308f51e7
dragomir <svedava@mail.ru>
2018-06-24 18:01:27 +0100

    Finished JS-Module webpack project and set up webpack.config for the WebpackProject

---
+++ js_modules/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+    publicPath: "build/"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        use: "babel-loader",
+        test: /\.js$/
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: { limit: 40000 }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
+
+module.exports = config;
