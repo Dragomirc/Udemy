33a073ac6b75c4dcd7f682fd7e9a94add90d294b
dragomir <svedava@mail.ru>
2018-07-22 21:21:56 +0100

    Lyrical-GraphQL added ApolloProvider and ApolloClient

---
+++ client/components/SongCreate.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+
+class SongCreate extends Component {
+  state = {
+    title: ""
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    this.props.mutate({
+      variables: {
+        title: this.state.title
+      }
+    });
+    this.props.history.push("/");
+  };
+  onInputChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+  render() {
+    console.log("SongCreate", this.props.data);
+    return (
+      <div>
+        <form onSubmit={this.onFormSubmit}>
+          <label htmlFor="songInput">Add Song</label>
+          <input
+            onChange={this.onInputChange}
+            type="text"
+            id="songInput"
+            name="title"
+            value={this.state.title}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      id
+      title
+    }
+  }
+`;
+export default graphql(mutation)(SongCreate);
--- server/server.js
+++ server/server.js
@@ -1,33 +1,38 @@
-const express = require('express');
-const models = require('./models');
-const expressGraphQL = require('express-graphql');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const schema = require('./schema/schema');
+const express = require("express");
+const models = require("./models");
+const expressGraphQL = require("express-graphql");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const schema = require("./schema/schema");
 
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI =
+  "mongodb://dragomir1:dragomir1@ds141631.mlab.com:41631/lyricaldbb";
+// "mongodb://dragomirc1:dragomirc1@ds147361.mlab.com:47361/lyricsdbase";
 if (!MONGO_URI) {
-  throw new Error('You must provide a MongoLab URI');
+  throw new Error("You must provide a MongoLab URI");
 }
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+  .once("open", () => console.log("Connected to MongoLab instance."))
+  .on("error", error => console.log("Error connecting to MongoLab:", error));
 
 app.use(bodyParser.json());
-app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true
-}));
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema,
+    graphiql: true
+  })
+);
 
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('../webpack.config.js');
+const webpackMiddleware = require("webpack-dev-middleware");
+const webpack = require("webpack");
+const webpackConfig = require("../webpack.config.js");
 app.use(webpackMiddleware(webpack(webpackConfig)));
 
 module.exports = app;
