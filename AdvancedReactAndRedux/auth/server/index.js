b32af2452186b21a838be4d7182fac8dfb12c63c
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2018-09-04 18:26:36 +0100

    AdvancedReactAndRedux/auth/server added authentication to the server

--- AdvancedReactAndRedux/auth/server/index.js
+++ AdvancedReactAndRedux/auth/server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const router = require("./router");
 const app = express();
 
@@ -13,4 +14,13 @@ router(app);
 //Server Setup - get express talking to the outside world
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
+
+const MONGO_URI =
+  "mongodb://dragomir89:dragomir89@ds143932.mlab.com:43932/advancedreact";
+
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URI);
+mongoose.connection
+  .once("open", () => console.log("Connected to MongoLab instance"))
+  .on("error", error => console.log("Error connecting to MongoLab:", error));
 server.listen(port, () => console.log(`Server listening on ${port}`));
