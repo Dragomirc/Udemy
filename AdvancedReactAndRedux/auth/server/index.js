26a4bf48121f03ab6fdd82de5b9e0dd9d393618e
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2018-09-28 16:21:43 +0100

    set app testing suits and wrote the first tests for App.js

--- AdvancedReactAndRedux/auth/server/index.js
+++ AdvancedReactAndRedux/auth/server/index.js
@@ -4,11 +4,13 @@ const http = require("http");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const router = require("./router");
 const app = express();
 
 //App Setup - get express working
 app.use(morgan("combined")); // used for debuging to console.log incoming requests
+app.use(cors());
 app.use(bodyParser.json({ type: "*/*" })); // parse all incoming requests as they were json
 router(app);
 //Server Setup - get express talking to the outside world
---
+++ React-Mastering Test Driven Development/giftgiver/src/components/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { gifts: [] };
+  }
+  addGift = () => {
+    const { gifts } = this.state;
+    const ids = gifts.map(gift => gift.id);
+    this.setState((state, props) => ({
+      gifts: [...state.gifts, { id: Math.max(...ids, 0) + 1 }]
+    }));
+  };
+  render() {
+    return (
+      <div>
+        <h2>Gift Giver</h2>
+        <Button className="btn-add" onClick={this.addGift}>
+          Add Gift
+        </Button>
+      </div>
+    );
+  }
+}
+export default App;
