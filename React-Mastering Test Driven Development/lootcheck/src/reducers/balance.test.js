08fef0b16db34f88c24336f3d5749dac5426675d
dragomir <svedava@mail.ru>
2018-11-04 20:46:45 +0000

    Test : added the balance to be stored in a cookie in localStorage

--- React-Mastering Test Driven Development/lootcheck/src/reducers/balance.test.js
+++ React-Mastering Test Driven Development/lootcheck/src/reducers/balance.test.js
@@ -1,16 +1,26 @@
 import balanceReducer from "./balance";
+import balanceReducer2 from "./balance";
+
 import * as constants from "../actions/constants";
 
 describe("balanceReducer", () => {
-  it("sets a balance", () => {
+  describe("when initializing", () => {
     const balance = 10;
-    expect(
-      balanceReducer(undefined, {
-        type: constants.SET_BALANCE,
-        payload: balance
-      })
-    ).toBe(balance);
+    it("sets a balance", () => {
+      expect(
+        balanceReducer(undefined, {
+          type: constants.SET_BALANCE,
+          payload: balance
+        })
+      ).toBe(balance);
+    });
+    describe("then re-initializing", () => {
+      it("reades the balance from cookies", () => {
+        expect(balanceReducer2(undefined, {})).toEqual(balance);
+      });
+    });
   });
+
   it("deposits into the balance", () => {
     const deposit = 10;
     const initialState = 5;
