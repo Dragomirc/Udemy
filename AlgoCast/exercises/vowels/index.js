ce4c61f1a4fac52548d1d58da44b019fad3c6e6b
dragomir <svedava@mail.ru>
2019-03-02 14:43:01 +0000

    AlgoCast solution for vowels(str)

--- AlgoCast/exercises/vowels/index.js
+++ AlgoCast/exercises/vowels/index.js
@@ -7,6 +7,37 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
-
+function vowels(
+  str,
+  vowelsArray = ["a", "e", "i", "o", "u"],
+  count = 0,
+  index = 0
+) {
+  if (index === str.length) {
+    return count;
+  }
+  if (vowelsArray.indexOf(str.charAt(index).toLowerCase()) !== -1) {
+    count++;
+  }
+  return vowels(str, vowelsArray, count, index + 1);
+}
 module.exports = vowels;
+// SOLUTION 1
+// function vowels(str) {
+//   const regex = /[aeiou]/gi;
+//   const matches = str.match(regex);
+//   return matches ? matches.length : 0;
+// }
+
+//SOLUTION 2
+// function vowels(str) {
+//   const vowelsArray = ["a", "e", "i", "o", "u"];
+//   let count = 0;
+
+//   for (let char of str) {
+//     if (vowelsArray.indexOf(char.toLowerCase()) !== -1) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
