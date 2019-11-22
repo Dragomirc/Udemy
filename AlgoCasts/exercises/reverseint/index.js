77b46dddbc33adbccefbf22dfea084db77d54278
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2019-11-22 11:50:50 +0000

    AlgoCast added solution for the reverseInt

--- AlgoCasts/exercises/reverseint/index.js
+++ AlgoCasts/exercises/reverseint/index.js
@@ -8,6 +8,19 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let nString = n.toString();
+  const isNumberNegative = nString[0] === '-';
+  let sum = 0;
+  if (isNumberNegative) {
+    nString = nString.substring(1);
+  }
+  for (let i = nString.length - 1; i >= 0; i--) {
+    sum += nString[i] * 10 ** i;
+  }
+  return isNumberNegative ? -sum : sum;
+}
+
+reverseInt(-5);
 
 module.exports = reverseInt;
