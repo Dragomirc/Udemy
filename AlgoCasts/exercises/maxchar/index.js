92d7dfddf405bc199830911ac5f7c30ff5bc909c
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2019-11-22 12:45:24 +0000

    added solution for the maxChar

--- AlgoCasts/exercises/maxchar/index.js
+++ AlgoCasts/exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const tempObj = {};
+  for (let i = 0; i < str.length; i++) {
+    const currChar = str.charAt(i);
+    if (tempObj[currChar]) {
+      tempObj[currChar] = tempObj[currChar] + 1;
+    } else {
+      tempObj[currChar] = 0;
+    }
+  }
+  const keysArray = Object.keys(tempObj);
+  let maxChar = keysArray[0];
+  let maxValue = tempObj[maxChar];
+  for (let i = 0; i < keysArray.length; i++) {
+    const currValue = tempObj[keysArray[i]];
+    if (currValue > maxValue) {
+      maxChar = keysArray[i];
+      maxValue = tempObj[maxChar];
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
