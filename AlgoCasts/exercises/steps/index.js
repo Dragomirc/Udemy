fbba2c7bc7b93d7a5204531041b254fc6489db6f
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2019-11-26 17:15:40 +0000

    AlgoCast added solution for the steps

--- AlgoCasts/exercises/steps/index.js
+++ AlgoCasts/exercises/steps/index.js
@@ -17,6 +17,29 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+/**1st solution */
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let string = '';
+//     for (let col = 0; col < n; col++) {
+//       string += col <= row ? '#' : ' ';
+//     }
+//     console.log(string);
+//   }
+// }
 
+/**2ns solution */
+function steps(n, row = 0, col = 0, string = '') {
+  if (row === n) {
+    return;
+  }
+
+  if (col < n) {
+    string += col <= row ? '#' : ' ';
+    return steps(n, row, col + 1, string);
+  }
+  console.log(string);
+  steps(n, row + 1);
+}
+steps(3);
 module.exports = steps;
