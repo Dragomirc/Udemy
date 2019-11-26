a11aeae88f6ddd701c9bdc5759ff2c153bede3bc
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2019-11-26 14:29:32 +0000

    Algocast added more solutions to the chunk problem

--- AlgoCasts/exercises/chunk/index.js
+++ AlgoCasts/exercises/chunk/index.js
@@ -9,17 +9,42 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 /** 1st solution */
+// function chunk(array, size) {
+//   const resArr = [];
+//   const copyArr = [...array];
+//   for (let i = 0, k = 0; i < copyArr.length; i += size, k++) {
+//     const newElem = [];
+//     for (let j = 0; j < size; j++) {
+//       if (copyArr[i + j]) newElem[j] = copyArr[i + j];
+//     }
+//     resArr[k] = [...newElem];
+//   }
+//   return resArr;
+// }
+
+/**2nd solution */
+// function chunk(array, size) {
+//   const chunked = [];
+//   for (let element of array) {
+//     const last = chunked[chunked.length - 1];
+//     if (!last || last.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+//   return chunked;
+// }
+
+/**3rd solution */
 function chunk(array, size) {
-  const resArr = [];
-  const copyArr = [...array];
-  for (let i = 0, k = 0; i < copyArr.length; i += size, k++) {
-    const newElem = [];
-    for (let j = 0; j < size; j++) {
-      if (copyArr[i + j]) newElem[j] = copyArr[i + j];
-    }
-    resArr[k] = [...newElem];
+  const chunked = [];
+  let index = 0;
+  while (index < array.length) {
+    chunked.push(array.slice(index, size + index));
+    index += size;
   }
-  return resArr;
+  return chunked;
 }
 
 chunk([1, 2, 3, 4], 2);
