091b4946b461cc5468581c380ef2d94c7aa29eb0
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2019-11-27 15:39:26 +0000

    AlgoCast added recursive solution for the pyramid

--- AlgoCasts/exercises/pyramid/index.js
+++ AlgoCasts/exercises/pyramid/index.js
@@ -14,6 +14,41 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+/**1st solution */
+// function pyramid(n) {
+//   const colLen = n + n - 1;
+//   const midStair = Math.floor(colLen / 2);
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+//     for (let col = 0; col < colLen; col++) {
+//       if (col >= midStair - row && col <= midStair + row) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
 
+/**2nd solution */
+function pyramid(n, stair = '', row = 0) {
+  if (row === n) {
+    return;
+  }
+  const colLen = n + n - 1;
+  const midStair = Math.floor(colLen / 2);
+  const col = stair.length;
+  if (col < colLen) {
+    if (col >= midStair - row && col <= midStair + row) {
+      stair += '#';
+    } else {
+      stair += ' ';
+    }
+    return pyramid(n, stair, row);
+  }
+  console.log(stair);
+  pyramid(n, '', row + 1);
+}
+pyramid(3);
 module.exports = pyramid;
