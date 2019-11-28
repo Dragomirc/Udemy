4601eecf97c147448acee3fdf8a737ec64b1c6d5
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2019-11-28 08:12:14 +0000

    AlgoCasts added solution for the matrix

--- AlgoCasts/exercises/matrix/index.js
+++ AlgoCasts/exercises/matrix/index.js
@@ -15,6 +15,41 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
-
+function matrix(n) {
+  const resArr = [];
+  let index = 0;
+  while (index < n) {
+    resArr.push([]);
+    index++;
+  }
+  let start = 1;
+  let startRow = 0;
+  let endRow = n - 1;
+  let startCol = 0;
+  let endCol = n - 1;
+  while (startRow <= endRow && startCol <= endCol) {
+    for (let i = startCol; i <= endCol; i++) {
+      resArr[startRow][i] = start;
+      start++;
+    }
+    startRow++;
+    for (let i = startRow; i <= endRow; i++) {
+      resArr[i][endCol] = start;
+      start++;
+    }
+    endCol--;
+    for (let i = endCol; i >= startCol; i--) {
+      resArr[endRow][i] = start;
+      start++;
+    }
+    endRow--;
+    for (let i = endRow; i >= startRow; i--) {
+      resArr[i][startCol] = start;
+      start++;
+    }
+    startCol++;
+  }
+  return resArr;
+}
+matrix(3);
 module.exports = matrix;
