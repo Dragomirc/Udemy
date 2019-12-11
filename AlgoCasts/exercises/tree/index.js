d148b331a81e56b7e9f933b1f17242bd41a0c7c9
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2019-12-11 18:24:33 +0000

    AlgoCasts added solution for Tree class

--- AlgoCasts/exercises/tree/index.js
+++ AlgoCasts/exercises/tree/index.js
@@ -10,8 +10,45 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
 
-class Tree {}
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    const index = this.children.findIndex(node => node.data === data);
+    this.children.splice(index, 1);
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  traverseBF(fn) {
+    const array = [this.root];
+
+    while (array.length) {
+      const currNode = array.shift();
+      array.push(...currNode.children);
+      fn(currNode);
+    }
+  }
+
+  traverseDF(fn) {
+    const array = [this.root];
+    while (array.length) {
+      const currNode = array.shift();
+      array.unshift(...currNode.children);
+      fn(currNode);
+    }
+  }
+}
 
 module.exports = { Tree, Node };
