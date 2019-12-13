d02c2bfd524ef2e658d59b555c8f667a94220d2c
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2019-12-13 14:30:16 +0000

    AlgoCasts added solution to mergeSort

--- AlgoCasts/exercises/sorting/index.js
+++ AlgoCasts/exercises/sorting/index.js
@@ -31,8 +31,31 @@ function selectionSort(arr) {
   return arr;
 }
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+  return merge(mergeSort(left), mergeSort(right));
+}
 
-function merge(left, right) {}
+function merge(left, right) {
+  let mergedArray = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      mergedArray.push(left.shift());
+    } else {
+      mergedArray.push(right.shift());
+    }
+  }
+  mergedArray = [...mergedArray, ...left, ...right];
+  return mergedArray;
+}
 
+function getArray() {
+  return [100, -40, 500, -124, 0, 21, 7];
+}
+mergeSort(getArray());
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
