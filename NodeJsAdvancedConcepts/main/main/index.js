3789c49cebc05772093f3bf65f46aba9fb196aaa
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2019-12-23 15:54:35 +0000

    NodeJsAdvancedCocepts seeting up the cluster test

---
+++ NodeJsAdvancedConcepts/main/main/index.js
@@ -0,0 +1,25 @@
+process.env.UV_THREADPOOL_SIZE = 1;
+const cluster = require('cluster');
+
+// Is file being executed in master mode?
+if (cluster.isMaster) {
+  //Cause index.js to be executed againt but in child mode
+  cluster.fork();
+} else {
+  //I'm a child and going to act a server and nothing else
+  const express = require('express');
+  const app = express();
+
+  app.get('/', (req, res) => {
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      res.send('Hi There');
+    });
+  });
+
+  app.get('/fast', (req, res) => {
+    res.send('This was fast!');
+  });
+  app.listen(3000, () => {
+    console.log('server listens on port 3000');
+  });
+}
