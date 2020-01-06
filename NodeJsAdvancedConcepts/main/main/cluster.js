7f8955571b2b7a7f26fe421e02254575d6b2626d
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2020-01-06 17:34:45 +0000

    NodeJsAdvnacedConcepts added myFile showing how node works

--- NodeJsAdvancedConcepts/main/main/cluster.js
+++ NodeJsAdvancedConcepts/main/main/cluster.js
@@ -1,5 +1,18 @@
+process.env.UV_THREADPOOL_SIZE = 1;
+const cluster = require('cluster');
+
+// Is file being executed in master mode?
+// if (cluster.isMaster) {
+//Cause index.js to be executed againt but in child mode
+// cluster.fork();
+// cluster.fork();
+// cluster.fork();
+// cluster.fork();
+// } else {
 //I'm a child and going to act a server and nothing else
+
 const express = require('express');
+const crypto = require('crypto');
 const app = express();
 
 app.get('/', (req, res) => {
@@ -14,3 +27,4 @@ app.get('/fast', (req, res) => {
 app.listen(3000, () => {
   console.log('server listens on port 3000');
 });
+// }
